1074edcefed8be663824d399edc12e4f1d314833
David Cruwys <david@ideasmen.com.au>
2022-11-28 13:29:55 +0700

    chore: github actions (ci/cd)

--- .builders/generators/01-bootstrap.rb
+++ .builders/generators/01-bootstrap.rb
@@ -72,7 +72,7 @@ KManager.action :bootstrap do
         run_command("git add .; git commit -m 'chore: #{self.options.description.downcase}'; git push")
       end
       .blueprint(
-        active: true,
+        active: false,
         name: :opinionated,
         description: 'opinionated GEM files',
         on_exist: :write) do
@@ -105,18 +105,18 @@ KManager.action :bootstrap do
         run_command("git add .; git commit -m 'chore: #{self.options.description.downcase}'; git push")
       end
       .blueprint(
-        active: false,
+        active: true,
         name: :ci_cd,
         description: 'github actions (CI/CD)',
         on_exist: :write) do
 
         cd(:app)
 
-        run_command("gh secret set SLACK_WEBHOOK --body \"$SLACK_REPO_WEBHOOK\"")
-        run_command("gh secret set GEM_HOST_API_KEY --body \"$GEM_HOST_API_KEY\"")
+        # run_command("gh secret set SLACK_WEBHOOK --body \"$SLACK_REPO_WEBHOOK\"")
+        # run_command("gh secret set GEM_HOST_API_KEY --body \"$GEM_HOST_API_KEY\"")
 
-        add('.github/workflows/main.yml')
-        add('.releaserc.json')
+        # add('.github/workflows/main.yml')
+        # add('.releaserc.json')
 
         run_command("git add .; git commit -m 'chore: #{self.options.description.downcase}'; git push")
       end
