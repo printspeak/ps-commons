9719f7142bb308fd35d889dad254003391f7800c
David Cruwys <david@ideasmen.com.au>
2022-12-16 17:36:16 +0700

    fix: cops

--- spec/ps/commons/queries/aggregate_query_builder_spec.rb
+++ spec/ps/commons/queries/aggregate_query_builder_spec.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class Address < ::ActiveRecord::Base
+class Address < ActiveRecord::Base
   establish_connection(adapter: 'sqlite3', database: ':memory:')
 
   connection.create_table :addresses do |t|
